830c733c5a9551b3e25d7a79abcd00d2ed9f3483
Vlad Spreys <vlad@spreys.com>
2016-04-24 14:55:21 +1200

    Adds servlet

---
+++ src/model/GameResult.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by vspreys on 24/04/16.
+ */
+public class GameResult implements Serializable {
+
+    private int id;
+    private int winnerId;
+    private int playerId;
+    private Date date;
+
+    public GameResult(Player player, Cat winner) {
+        this.playerId = player.getId();
+        this.winnerId = winner.getId();
+        this.date = new Date(); //Current date and time;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getWinnerId() {
+        return this.winnerId;
+    }
+
+    public int getPlayerId() {
+        return this.playerId;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+}
