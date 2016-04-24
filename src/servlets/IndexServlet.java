841e4328f5b74d52a6041b53596f79d8f8c0bc0c
Vlad Spreys <vlad@spreys.com>
2016-04-24 15:20:49 +1200

    Adds the first jsp.

---
+++ src/servlets/IndexServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import model.Player;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by vspreys on 24/04/16.
+ */
+@WebServlet(name = "IndexServlet", urlPatterns = {"/"})
+public class IndexServlet extends HttpServlet {
+    private static final String ATTRIBUTE_PLAYER = "player";
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        HttpSession session = request.getSession(true); //Create session if doesn't exist
+
+        Player player = (Player) session.getAttribute(ATTRIBUTE_PLAYER);
+
+        if(player == null) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/registration.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+}
---
+++ web/registration.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vspreys
+  Date: 24/04/16
+  Time: 3:17 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome to cat fights!</title>
+</head>
+<body>
+<h1> Welcome to cat fights!</h1>
+</body>
+</html>
